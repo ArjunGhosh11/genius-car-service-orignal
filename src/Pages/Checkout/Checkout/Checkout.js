4f1b016a9f1d88f059a250e431fb7aa26ee3193e
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-04-28 18:59:52 +0600

    order connected to server

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useServiceDetail from '../../../hooks/useServiceDetail';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 const Checkout = () => {
+    const [user] = useAuthState(auth)
     const { serviceId } = useParams();
     const [service] = useServiceDetail(serviceId);
+
+    const handlePlaceOrder = (event) => {
+        event.preventDefault();
+        const order = {
+            email: user.email,
+            service: service.name,
+            serviceId: serviceId,
+            address: event.target.address.value,
+            phone: event.target.phone.value
+        }
+    }
     return (
         <div className='w-50 mx-auto'>
             <h2>Please order: {service.name}</h2>
-            <form>
-                <input type="text" name='name' placeholder='Name' required />
+            <form onSubmit={handlePlaceOrder}>
+                <input className='w-100 mb-2' type="text" name='name' value={user.displayName} placeholder='Name' required readOnly disabled />
+                <br />
+                <input className='w-100 mb-2' type="email" name='email' value={user.email} placeholder='Email' required readOnly disabled />
+                <br />
+                <input className='w-100 mb-2' type="text" name='service' placeholder='Service' value={service.name} required />
+                <br />
+                <input className='w-100 mb-2' type="text" name='address' placeholder='Address' required autoComplete='off' />
+                <br />
+                <input className='w-100 mb-2' type="text" name='phone' placeholder='Phone' required />
+                <br />
+                <input className='btn btn-primary' type="submit" value="Place Order" />
             </form>
         </div>
     );
