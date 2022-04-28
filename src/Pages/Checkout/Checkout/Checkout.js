f2718cb81081da9334efb74e432dc71150b80909
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-04-28 20:23:16 +0600

    Checkout added

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import useServiceDetail from '../../../hooks/useServiceDetail';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 const Checkout = () => {
     const [user] = useAuthState(auth)
     const { serviceId } = useParams();
@@ -17,16 +19,24 @@ const Checkout = () => {
             address: event.target.address.value,
             phone: event.target.phone.value
         }
+        axios.post('http://localhost:5000/order', order)
+            .then(response => {
+                const { data } = response;
+                if (data.insertedId) {
+                    toast('Your order is booked!!');
+                    event.target.reset();
+                }
+            })
     }
     return (
         <div className='w-50 mx-auto'>
             <h2>Please order: {service.name}</h2>
             <form onSubmit={handlePlaceOrder}>
-                <input className='w-100 mb-2' type="text" name='name' value={user.displayName} placeholder='Name' required readOnly disabled />
+                <input className='w-100 mb-2' type="text" name='name' value={user?.displayName} placeholder='Name' required readOnly disabled />
                 <br />
-                <input className='w-100 mb-2' type="email" name='email' value={user.email} placeholder='Email' required readOnly disabled />
+                <input className='w-100 mb-2' type="email" name='email' value={user?.email} placeholder='Email' required readOnly disabled />
                 <br />
-                <input className='w-100 mb-2' type="text" name='service' placeholder='Service' value={service.name} required />
+                <input className='w-100 mb-2' type="text" name='service' placeholder='Service' value={service.name} required readOnly />
                 <br />
                 <input className='w-100 mb-2' type="text" name='address' placeholder='Address' required autoComplete='off' />
                 <br />
